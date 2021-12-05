4088a6eb5ef5e1d96659cb82893bbb5e0ac3aa44
Heisenberg <1157109110@qq.com>
2021-12-05 16:41:00 +0800

    main

--- 继承/继承.js
+++ 继承/继承.js
@@ -6,7 +6,7 @@ function Son1(){
     this.name = 'son'
 };
 Son1.prototype = new Father();
-//缺点:1.不能向构造函数传递参数 2.所有实例共享父类原型上的属性;
+//缺点:1.不能向构造函数传递参数 2.所有实例共享父类原型上的属性,在包含有引用类型的数据时，会被所有的实例对象所共享，容易造成修改的混乱;
 
 //构造函数继承
 function Son2(){;
@@ -29,10 +29,28 @@ function Child(){
     Father.call(this)
     this.name = 'child'
 };
-//寄生
 Child.prototype =Object.create(Father.prototype);
 Child.prototype.constructor = Child;
 const kid = new Child();
 console.log(kid.__proto__.__proto__===Father.prototype);
 let num = Number.MAX_SAFE_INTEGER
 console.log(num);
+
+//原型继承
+function object(o){
+    function F(){};
+    F.prototype = o;
+    return new F()
+}
+//基于已有的对象来创建新的对象
+//原型式继承非常适合不需要单独创建构造函数，但仍然需要在对象间共享信息的场合。
+
+//寄生继承
+function inhert(o){
+    let clone = object(o);//创建一个新对象
+    clone.b = 222;//以某种方式增强新对象;
+    return clone;//返回对象
+};
+let o = {a:1};
+let o1 = object(o);
+console.log(o1.a);
