219e87cac02e1e58f1b26caf9412dc802169c26b
Heisenberg <1157109110@qq.com>
2021-12-17 01:02:19 +0800

    main

---
+++ 手写js/27解析param.js
@@ -0,0 +1,28 @@
+//'http://www.domain.com/?user=anonymous&id=123&id=456&city=%E5%8C%97%E4%BA%AC&enabled';
+function parseUrl(url){
+    let obj = {}
+    let paramStr = url.split('?')[1];
+    console.log(paramStr);
+    let arr = paramStr.split('&');
+    console.log(arr);
+    for(let i =0;i<arr.length;i++){
+        arr[i] = arr[i].split('=');
+        let [key,val] = arr[i];//解构赋值
+        val = decodeURIComponent(val)//解码
+        if(key in obj&&arr.length>1){
+            obj[key] = [].concat(obj[key],val)
+            // if(Array.isArray(obj[key])){
+            //     obj[key].push(val)
+            // }else{
+            //     obj[key] = [obj[key],val]
+            // }
+        }else{
+            obj[key] = val?val:true
+        }
+    }
+    console.log(arr);
+    return obj
+};
+// console.log(parseUrl('http://www.domain.com/?user=anonymous&id=123&id=456&city=%E5%8C%97%E4%BA%AC&enabled'));
+
+console.log('00001'*1);
