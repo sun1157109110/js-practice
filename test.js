612c7b8b1a72a47688b54ff1d9151c459eb65e81
sunyun <sun1157109110@163.com>
2022-07-11 21:51:22 +0800

    update

--- test.js
+++ test.js
@@ -1,57 +1,15 @@
-// async function test(){
-//    const a = await Promise.reject('@');
-//    console.log(a);
-// };
-// test()
-// console.log(`[2]['to'].coupang`.replace(/\[(\d+)\]/g, '.$1'));
-// const fs = require('fs')
-// fs.readFile('./1.txt','utf-8',(err,dataStr)=>{
-//   console.log(err);
-//   console.log(dataStr);
-// })
-// console.log(new Date().toString());
-// console.log('-------------------');
-// console.log(new Date().toDateString());
-// console.log('-------------------');
-// console.log(new Date().toLocaleString());
-// console.log('-------------------');
-// console.log(Date.now());
-// try {
-//   throw new Error('eeeeee')
-// } catch (error) {
-//   console.log(error.message);
-// }finally{
-//   console.log(111);
-// }
-// console.log('155'.padStart(7,'0'));
-// console.log(new Date('2022-5-14 03:07:45').getTime());
-// const obj = {hh:'aaa'}
-// function a(aa,bb){
-//   console.log(aa,bb);
-// }
-// const arr = {c:44,d:55}
-// a(...arr)
-// function *test(){
-//   yield 1;
-//   yield 2;
-//   return 3
-// }
-// const handle = test();
-// console.log(console.log(handle));
-// console.log(handle.next());
-// console.log(handle.next());
+function randomColor() {
+  var i = 0;
+  var str = "#";
+  var random = 0;
+  var aryNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-// async function a(params) {
-//  try {
-//   await Promise.reject('@@@')
-//   await Promise.reject('!!!')
-//  } catch (error) {
-//    console.log(error);
-//  }
-// }
-// a()
-async function ccc(params) {
-  const a = await Promise.reject('@')
-  console.log(a);
+  for (i = 0; i < 6; i++) {
+    random = parseInt(Math.random() * 16);
+
+    str += aryNum[random];
+  }
+
+  console.log(str);
 }
-ccc()
+randomColor()
---
+++ 手写js/lodashget.js
@@ -0,0 +1,46 @@
+//实现_.get()
+/**
+ * Gets the value at `path` of `object`. If the resolved value is
+ * `undefined`, the `defaultValue` is returned in its place.
+ *
+ * @since 3.7.0
+ * @category Object
+ * @param {Object} object The object to query.
+ * @param {Array|string} path The path of the property to get.
+ * @param {*} [defaultValue] The value returned for `undefined` resolved values.
+ * @returns {*} Returns the resolved value.
+ * @see has, hasIn, set, unset
+ * @example
+ *
+ * const object = { 'a': [{ 'b': { 'c': 3 } }] }
+ *
+ * get(object, 'a[0].b.c')
+ * // => 3
+ *
+ * get(object, ['a', '0', 'b', 'c'])
+ * // => 3
+ *
+ * get(object, 'a.b.c', 'default')
+ * // => 'default'
+ */
+function get(obj, path, defaultValue = undefined) {
+  //处理path 可能是字符串也可能是数组
+  if (typeof path === 'string') {
+    path = path.replace(/\[(\d+)\]/, '.$1').split('.')
+  } else if (!Array.isArray(path)) {
+    return defaultValue
+  }
+  let res = obj
+  for (let key of path) {
+    res = Object(res)[key]
+    if (res === undefined) return defaultValue
+  }
+  return res
+}
+const object = {
+  'a': [{
+    'b': {
+      'c': 3
+    }
+  }]
+}
