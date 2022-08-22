01ac5a830a4b83fd78df1612113f278f63d082eb
sunyun <sun1157109110@163.com>
2022-08-22 23:56:47 +0800

    change

--- 手写js/20Promise.js
+++ 手写js/20Promise.js
@@ -268,48 +268,4 @@ addTask(400, '4')
 scheduler.taskStart()
 // output: 2 3 1 4
 
-//实现promise串行函数
-function serialPromise(array) {
-  let res = []
-  return new Promise((resolve, reject) => {
-    array
-      .reduce((pre, cur) => {
-        return pre.then(cur).then((value) => {
-          res.push(value)
-        })
-      }, Promise.resolve())
-      .then(() => {
-        resolve(res)
-      })
-  })
-}
 
-//promise.all并行请求限制
-function multiRequest(urls, maxNum) {
-  const len = urls.length
-  let count = 0
-  const res = Array(len).fill(false)
-  while (count < maxNum) {
-    next()
-  }
-  function next() {
-    count++
-    return new Promise((resolve, reject) => {
-      if (count >= len) {
-        !res.includes(false) && resolve(res)
-      }
-      console.log(`开始${count},${new Date().toLocaleString()}`)
-      fetch(urls[count])
-        .then((value) => {
-          res[count] = value
-          console.log(`完成${count},${new Date().toLocaleString()}`)
-          if (count < maxNum) next()
-        })
-        .catch((err) => {
-          res[count] = err
-          console.log(`结束${count},${new Date().toLocaleString()}`)
-          if (count < maxNum) next()
-        })
-    })
-  }
-}
